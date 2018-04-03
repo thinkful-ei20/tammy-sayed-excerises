2b1b978825c038b9412d13b07d5864a7d7df8e0e
Sayed Khan <arsalon47@gmail.com>
2018-04-03 12:22:56 -0400

    FaA1

--- main.js
+++ main.js
@@ -40,3 +40,22 @@ function findAverage(array) {
 }
 
 // findAverage(numbersArray);
+
+function repeat(fn, n) {
+  let i = 0;
+  while (i < n) {
+    fn();
+    i++;
+  }
+}
+
+function hello() {
+  console.log('Hello world');
+}
+
+function goodbye() {
+  console.log('Goodbye world');
+}
+
+// repeat(hello, 5);
+// repeat(goodbye, 5);
